ae93966e758362079cedde94c9eaffed42cbfceb
douglasoliv <38958217+douglasoliv@users.noreply.github.com>
2018-05-03 18:55:57 -0300

    Interações Básicas
    Modificação das interações básicas,
    adicionando interação com as views "CadastroActivity" e "ListaProdutosActivity"

--- app/src/main/java/com/example/android/mercadoagil/LoginActivity.java
+++ app/src/main/java/com/example/android/mercadoagil/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.mercadoagil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements AdapterView.OnClickListener {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +17,54 @@ public class LoginActivity extends AppCompatActivity implements AdapterView.OnCl
         setContentView(R.layout.activity_login);
 
         Button btnAcessar = findViewById(R.id.buttonLogin);
-        //btnAcessar.setOnClickListener();
+        btnAcessar.setOnClickListener(Acessar());
 
-        if (validaLogin()) {
-            Toast.makeText(this, "Login aceito.", Toast.LENGTH_LONG);
-        }
+        Button btNovo = findViewById(R.id.buttonCadastro);
+        btNovo.setOnClickListener(Cadastrar());
+
+    }
+
+    private void alet(String s) {
+
+        Toast.makeText(this,s , Toast.LENGTH_LONG).show();
+    }
+
+    private View.OnClickListener Acessar() {
+        return new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (validaLogin()) {
+                    alet("Sucesso");
+
+                   Intent mudar = new Intent(LoginActivity.this,ListaProdutosActivity.class);
+                   LoginActivity.this.startActivity(mudar);
+                }
+                else alet("Usuario incorreto");
+            }
+        };
+    }
+    private View.OnClickListener Cadastrar() {
+        return new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent mudarTela = new Intent(LoginActivity.this,CadastroActivity.class);
+                LoginActivity.this.startActivity(mudarTela);
+
+            }
+        };
     }
 
     public boolean validaLogin(){
+
         EditText Login = findViewById(R.id.campoLogin);
         EditText Senha = findViewById(R.id.campoSenha);
 
-        if (Login.toString() == "abc" && Senha.toString() == "123") {
+        String tLogin = Login.getText().toString();
+        String tSenha = Senha.getText().toString();
+
+        if ("abc".equals(tLogin) && "123".equals(tSenha)) {
             return true;
         }
         else {
@@ -35,8 +72,5 @@ public class LoginActivity extends AppCompatActivity implements AdapterView.OnCl
         }
     }
 
-    @Override
-    public void onClick(View view) {
 
-    }
 }
