31d32f52ad21f387f57ec58f34c03bb4eac62200
douglasoliv <38958217+douglasoliv@users.noreply.github.com>
2018-06-11 00:50:22 -0300

    Activity setores

---
+++ app/src/main/java/com/example/android/mercadoagil/SetoresActivity.java
@@ -0,0 +1,42 @@
+package com.example.android.mercadoagil;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.example.android.mercadoagil.adapter.SetorAdapter;
+
+public class SetoresActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    private GridView gridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+
+        int[] imagens = {R.drawable.ic_cutlery,
+                R.drawable.ic_cutlery,
+                R.drawable.ic_cutlery,
+                R.drawable.ic_cutlery,
+                R.drawable.ic_cutlery,
+                R.drawable.ic_cutlery};
+
+        setContentView(R.layout.activity_setores);
+        gridView = (GridView) findViewById(R.id.grid1) ;
+        gridView.setAdapter(new SetorAdapter(this, imagens));
+        gridView.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this,"Imagem selecionado: " + (position+1), Toast.LENGTH_SHORT).show();
+        Intent telaPosSetor = new Intent(SetoresActivity.this, ListaProdutosActivity.class);
+        SetoresActivity.this.startActivity(telaPosSetor);
+
+    }
+}
---
+++ app/src/main/java/com/example/android/mercadoagil/adapter/SetorAdapter.java
@@ -0,0 +1,45 @@
+package com.example.android.mercadoagil.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.example.android.mercadoagil.R;
+
+public class SetorAdapter extends BaseAdapter {
+
+    private Context context;
+    private final int[] imagens;
+
+    public SetorAdapter(Context context, int[] imagens){
+
+        this.context = context ;
+        this.imagens = imagens ;
+
+    }
+    @Override
+    public int getCount() {
+        return imagens.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = LayoutInflater.from(this.context).inflate(R.layout.layout_setor,parent,false);
+        ImageView image = (ImageView) view.findViewById(R.id.imageGrid) ;
+
+        return view;
+    }
+}
