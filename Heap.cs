cd81fbf740ed7a5e887ffe08807a59bc0c476515
Strayf <erikbrunnert@gmail.com>
2019-09-17 20:14:53 -0300

    Create Heap.cs

---
+++ Heap.cs
@@ -0,0 +1,107 @@
+using System;
+
+public class Heap<T> where T : IHeapItem<T>
+{
+    T[] items;
+    private int currentItemCount = 0;
+
+	public Heap(int maxHeapSize)
+	{
+        items = new T[maxHeapSize];
+	}
+
+    public int Count
+    {
+        get { return currentItemCount; }
+    }
+
+    public void Add(T item)
+    {
+        item.HeapIndex = currentItemCount;
+        items[currentItemCount] = item;
+        SortUp(item);
+        currentItemCount++;
+    }
+
+    public void RemoveFirst()
+    {
+        T firstItem = items[0];
+        currentItemCount--;
+        items[0] = items[currentItemCount];
+        items[0].HeapIndex = 0;
+        SortDown(items[0]);
+        return firstItem;
+    }
+
+    public UpdateItem(T item)
+    {
+        SortUp(item);
+    }
+
+    public bool Contains(T item)
+    {
+        return Equals(items[item.HeapIndex], item);
+    }
+
+    private void SortUp(T item)
+    {
+        int parentIndex = (item.HeapIndex - 1) / 2;
+
+        while (true)
+        {
+            T parentItem = items[parentIndex];
+            if (item.CompareTo(parentItem) > 0)
+            {
+                Swap(item, parentItem);
+            }
+            else
+                break;
+        }
+    }
+
+    private void SortDown(T item)
+    {
+        while (true)
+        {
+            int childIndexL = (item.HeapIndex * 2) + 1;
+            int childIndexR = (item.HeapIndex * 2) + 2;
+            int swapIndex = 0;
+
+            if (childIndexL < currentItemCount)
+            {
+                swapIndex = childIndexL;
+                if (childIndexR < currentItemCount)
+                {
+                    if (items[childIndexL].CompareTo(items[childIndexR]) < 0)
+                    {
+                        swapIndex = childIndexR;
+                    }
+                }
+
+                if (item.CompareTo(items[swapIndex]) < 0)
+                {
+                    Swap(item, items[swapIndex]);
+                }
+                else
+                    return;
+            }
+            else
+                return;
+        }
+    }
+
+    private void Swap(T itemA, T itemB)
+    {
+        items[itemA.HeapIndex] = itemA;
+        items[itemB.HeapIndex] = itemB;
+
+        int AIndex = itemA.HeapIndex;
+        itemA.HeapIndex = itemB.HeapIndex;
+        itemB.HeapIndex = AIndex;
+    }
+}
+
+public interface IHeapItem<T> : IComparable<T>
+{
+    int HeapIndex { get; set; }
+}
